67fcd4dd2bcc39ede4f9d756220b613916724a0b
Rahman Amarullah Siregar <rahman.amarullah@gmail.com>
2024-05-07 02:23:03 +0700

    activate storage link and add monitor.css

--- resources/views/layouts/monitor.blade.php
+++ resources/views/layouts/monitor.blade.php
@@ -6,16 +6,28 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title') | {{ config('app.name', 'My App') }}</title>
     <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+
+    <!-- My Css -->
+    <link id="pagestyle" href="../assets/css/monitor.css" rel="stylesheet" />
+    <link id="pagestyle" href="../assets/css/monitor-mobile.css" rel="stylesheet" />
+    <!-- My Css -->
+
+    <!-- Slick Carousel -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
+    <!-- Slick Carousel -->
+
+    {{-- <script src="https://kit.fontawesome.com/a031935a14.js" crossorigin="anonymous"></script> --}}
+    @yield('refresh-time')
 </head>
 <body>
-    <div class="main-container">
-        @yield('content')
-        @stack('scripts')
-    </div>
+    @yield('content')
+
+    @stack('scripts')
 </body>
 <footer>
     <div>
-        <p>Copyright &copy; 2020 Cekdos</p>
+        <p style="text-align: center">Copyright &copy; 2020 Cekdos</p>
     </div>
 </footer>
 </html>
--- resources/views/monitor/index.blade.php
+++ resources/views/monitor/index.blade.php
@@ -1,13 +1,41 @@
 @extends('layouts.monitor') <!-- Menggunakan Layout bernama "app.blade.php" pada direktori /views/layouts/ -->
 
 @section('title', 'List Dosens')
+@section('refresh-time')
+<meta http-equiv="refresh" content="{{ (count($dosens)*3)+5 }}">    {{-- Melakukan Auto Refresh sebanyak jumlah dosen --}}
+@endsection
 
 @section('content')
+<div class="main-container">
+    <div class="slider">
+        @foreach ($dosens as $dosen)
+            @switch($dosen->status_kehadiran)
+                @case('Hadir')
+                    <div class="hadir border">
+                    @break
+                @case('Tidak Hadir')
+                    <div class="absen border">
+                    @break
+                @case('Mengajar')
+                    <div class="mengajar border">
+                    @break
+                @default
 
-@foreach ($dosens as $dosen)
-    <div><h1>{{ $dosen->nama }}</h1></div>
-@endforeach
-
+            @endswitch
+                        <div class="text-center">
+                            <div >
+                                <h1 class="nama_dosen text-center">
+                                    {{ $dosen->nama }}
+                                </h1>
+                                <div class="status-kehadiran text-center">
+                                    <h2>Sedang {{ $dosen->status_kehadiran }}</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+        @endforeach
+    </div>
+</div>
 @endsection
 @push('scripts')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
@@ -19,7 +47,7 @@
             slidesToShow: 2,
             slidesToScroll: 2,
             infinite: false,    //true untuk slide berulang ke awal
-            autoplay: false,
+            autoplay: true,
             autoplaySpeed: 6000,
             dots: true,
             arrows: true,
